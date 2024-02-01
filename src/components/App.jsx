0d082a359650e590108ada89cc600d730c119035
YaroslavKyba <narxoc.ne@gmail.com>
2024-02-01 18:11:45 +0200

    1 step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,37 @@
-export const App = () => {
-  return <div>React homework template</div>;
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <label>
+          <span>Name</span>
+          <input type="text" name="name" required />
+        </label>
+        <label>
+          <span>Number</span>
+          <input type="tel" name="number" required />
+        </label>
+        <button type="submit">Add Contact</button>
+
+        <ul>Contacts</ul>
+        <li name="Rozie" id="dsf">
+          Rozie
+        </li>
+      </div>
+    );
+  }
+}
